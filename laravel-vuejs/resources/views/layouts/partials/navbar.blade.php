8df8eaf085d9e1246bae04e91c045e11a602f33a
Ken Duigraha Putra <kenduigraha@kudo.co.id>
2019-01-03 08:39:18 +0700

    Add dashboard menu in navbar.

--- laravel-vuejs/resources/views/layouts/partials/navbar.blade.php
+++ laravel-vuejs/resources/views/layouts/partials/navbar.blade.php
@@ -32,6 +32,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('dashboard') }}">
+                                {{ __('dashboard') }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
